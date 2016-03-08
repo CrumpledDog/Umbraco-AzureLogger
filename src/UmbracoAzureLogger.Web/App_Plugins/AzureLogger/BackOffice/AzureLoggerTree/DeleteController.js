ce10daa639fd7f486d7ea0a6fdef0831e412e9e4
Hendy Racher <git@hendyracher.co.uk>
2016-03-08 09:24:45 +0000

    http service injected

--- src/UmbracoAzureLogger.Web/App_Plugins/AzureLogger/BackOffice/AzureLoggerTree/DeleteController.js
+++ src/UmbracoAzureLogger.Web/App_Plugins/AzureLogger/BackOffice/AzureLoggerTree/DeleteController.js
@@ -1,8 +1,8 @@
 ﻿angular
     .module('umbraco')
     .controller('AzureLogger.DeleteController', [
-        '$scope',
-        function ($scope) {
+        '$scope', '$http',
+        function ($scope, $http) {
 
             /* scope vars */
 
