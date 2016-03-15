65609fc82197d1b54810d07b8c390271056c698c
Jeavon <jeavon@crumpled-dog.com>
2016-03-15 20:42:33 +0000

    Assembly info

--- src/Our.Umbraco.AzureLogger.Web/Properties/AssemblyInfo.cs
+++ src/Our.Umbraco.AzureLogger.Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UmbracoAzureLogger.Web")]
+[assembly: AssemblyTitle("Our.Umbraco.AzureLogger.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UmbracoAzureLogger.Web")]
+[assembly: AssemblyProduct("Our.Umbraco.AzureLogger.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
