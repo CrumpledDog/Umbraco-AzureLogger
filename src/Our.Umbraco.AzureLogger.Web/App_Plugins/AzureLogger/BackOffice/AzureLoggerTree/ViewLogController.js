85461a5c500703a6cd07c937a2493705a6fa7bfc
Hendy <git@hendyracher.co.uk>
2016-03-21 16:16:20 +0000

    moved the LazyLoad Angular directive into it's own js file

--- src/Our.Umbraco.AzureLogger.Web/App_Plugins/AzureLogger/BackOffice/AzureLoggerTree/ViewLogController.js
+++ src/Our.Umbraco.AzureLogger.Web/App_Plugins/AzureLogger/BackOffice/AzureLoggerTree/ViewLogController.js
@@ -14,11 +14,11 @@
                     //    navigationService.changeSection('developer');
 
                     //} else {
-                        $scope.searchItem = searchItem; // put into scope so view can delay rendering until populated
+                    $scope.searchItem = searchItem; // put into scope so view can delay rendering until populated
 
-                        $scope.logItems = [];
-                        $scope.finishedLoading = false;
-                        $scope.getMoreLogItems();
+                    $scope.logItems = [];
+                    $scope.finishedLoading = false;
+                    $scope.getMoreLogItems();
                     //}
 
                 });
@@ -42,10 +42,10 @@
             //$scope.startEventTimestamp; // set with date picker
             //$scope.threadIdentity; // built from AppDomainId + ProcessId + ThreadName (set by clicking in details view)
 
-//            $scope.logItems = [];
+            //            $scope.logItems = [];
             //$scope.logItemLimit = 1000; // size of logItems array before it is reset (and a new start date time in the filter)
-//            $scope.currentlyLoading = false; // true when getting data awaiting a response to set
-//            $scope.finishedLoading = false; // true once server indicates that there is no more data
+            //            $scope.currentlyLoading = false; // true when getting data awaiting a response to set
+            //            $scope.finishedLoading = false; // true once server indicates that there is no more data
 
 
             // get connection details (triggering a new connection if required)
@@ -130,43 +130,4 @@
                 return date.toDateString() !== lastDate.toDateString();
             };
 
-        }])
-        .directive('lazyLoad', ['$timeout', function ($timeout) {
-
-            return {
-                restrict: 'A',
-                link: function (scope, element, attrs) {
-
-                    // returns true if the element hasn't overlaped the bottom of the view
-                    var lazyLoad = function () {
-                        var overlap = 500;
-                        return (element.offset().top + element.height() < $(window).height() + overlap);
-                    };
-
-                    // initialize to ensure content is loaded to fill the initial screen (before any scroll activity)
-                    $timeout(function () {
-                        //TODO: check the lazy load has filled the screen
-                        if (lazyLoad()) {
-                            scope.$apply(attrs.lazyLoad);
-                        }
-                    });
-
-                    var scrollDelayTimer;
-                    // jQuery walk up tree from element to find div with class 'umb-scrollable', to hook into when this is scrolled
-                    $(element).closest('.umb-scrollable').bind('scroll', function () {
-
-                        // cancel any previous timer and set new one
-                        if (scrollDelayTimer) {
-                            $timeout.cancel(scrollDelayTimer);
-                        }
-
-                        scrollDelayTimer = $timeout(function () {
-                            if (lazyLoad()) {
-                                scope.$apply(attrs.lazyLoad);
-                            }
-                        }, 250);
-
-                    });
-                }
-            }
         }]);
---
+++ src/Our.Umbraco.AzureLogger.Web/App_Plugins/AzureLogger/BackOffice/LazyLoadDirective.js
@@ -0,0 +1,47 @@
+﻿angular
+    .module('umbraco')
+    .directive('lazyLoad', ['$timeout', function ($timeout) {
+
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+
+                // returns true if the element hasn't overlaped the bottom of the view
+                var lazyLoad = function () {
+                    var overlap = 500;
+
+                    console.log('scrolly');
+                    console.log(element.offset().top);
+                    console.log(element.height());
+                    console.log($(window).height());
+
+                    return (element.offset().top + element.height() < $(window).height() + overlap);
+                };
+
+                // initialize to ensure content is loaded to fill the initial screen (before any scroll activity)
+                $timeout(function () {
+                    //TODO: check the lazy load has filled the screen
+                    if (lazyLoad()) {
+                        scope.$apply(attrs.lazyLoad);
+                    }
+                });
+
+                var scrollDelayTimer;
+                // jQuery walk up tree from element to find div with class 'umb-scrollable', to hook into when this is scrolled
+                $(element).closest('.umb-scrollable').bind('scroll', function () {
+
+                    // cancel any previous timer and set new one
+                    if (scrollDelayTimer) {
+                        $timeout.cancel(scrollDelayTimer);
+                    }
+
+                    scrollDelayTimer = $timeout(function () {
+                        if (lazyLoad()) {
+                            scope.$apply(attrs.lazyLoad);
+                        }
+                    }, 250);
+
+                });
+            }
+        }
+    }]);
