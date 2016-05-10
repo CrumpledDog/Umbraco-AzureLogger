d2be0f88b8b100756cd9895c8c72575ab466764c
Hendy <git@hendyracher.co.uk>
2016-05-10 22:17:49 +0100

    skeleton autocomplete directive

---
+++ src/Our.Umbraco.AzureLogger.Web/App_Plugins/AzureLogger/BackOffice/Directives/AutoCompleteDirective.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('umbraco')
+        .directive('autoComplete', AutoCompleteDirective);
+
+    AutoCompleteDirective.$inject = ['$compile'];
+
+    function AutoCompleteDirective($compile) {
+
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+
+            }
+        }
+    }
+
+})();
