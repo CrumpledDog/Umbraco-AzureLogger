470ac5660ee9dcb7e50b05ee4a63915c19f1f27b
Hendy <git@hendyracher.co.uk>
2016-05-11 11:52:57 +0100

    rendering of autocomplete options

--- src/Our.Umbraco.AzureLogger.Web/App_Plugins/AzureLogger/BackOffice/Directives/AutoCompleteDirective.js
+++ src/Our.Umbraco.AzureLogger.Web/App_Plugins/AzureLogger/BackOffice/Directives/AutoCompleteDirective.js
@@ -10,8 +10,41 @@
     function AutoCompleteDirective($compile) {
 
         return {
+            require: 'ngModel',
             restrict: 'A',
-            link: function (scope, element, attrs) {
+            scope: '=',
+            //template: '<ul><li>example autocomplete option</li></ul>',
+            link: function (scope, element, attrs, ngModel) {
+
+                // ensure only attached to input element of type text
+                if (element[0].tagName.toLowerCase() !== 'input' || element[0].type !== 'text') { return; }
+
+                var optionsList = angular.element(
+                    '<ul style="position:absolute; left:0; top:' + $(element[0]).height() + 'px; z-index:1; background-color:white; list-style:none; margin:0; border:solid 1px #ccc;">' +
+                    '<li style="margin:5px" ng-repeat="option in getData()">{{option}}<li>' +
+                    '</ul>');
+
+                $compile(optionsList)(scope);
+
+                element.before(optionsList);
+
+                //ngModel.$parsers.unshift(function (viewValue) {
+                //    if (viewValue === 'foo') {
+                //        // revert to previous (model value)
+                //        var currentValue = ngModel.$modelValue;
+                //        ngModel.$setViewValue(currentValue);
+                //        ngModel.$render();
+                //        return currentValue;
+                //    }
+                //    else {
+                //        return viewValue;
+                //    }
+                //});
+
+                // return the expected array
+                scope.getData = function () {
+                    return scope.$eval(attrs.autoComplete);
+                }
 
             }
         }
