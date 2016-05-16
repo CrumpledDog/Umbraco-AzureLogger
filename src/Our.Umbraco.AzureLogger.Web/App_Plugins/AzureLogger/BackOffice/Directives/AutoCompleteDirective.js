6e8aaf8d35f2bdafcc27c147d3378471c4dcc194
Hendy <git@hendyracher.co.uk>
2016-05-16 23:44:17 +0100

    toggle visibility of auto complete list

--- src/Our.Umbraco.AzureLogger.Web/App_Plugins/AzureLogger/BackOffice/Directives/AutoCompleteDirective.js
+++ src/Our.Umbraco.AzureLogger.Web/App_Plugins/AzureLogger/BackOffice/Directives/AutoCompleteDirective.js
@@ -19,14 +19,27 @@
                 // ensure only attached to input element of type text
                 if (element[0].tagName.toLowerCase() !== 'input' || element[0].type !== 'text') { return; }
 
+                scope.show = false;
+
+                // build list markup
                 var optionsList = angular.element(
-                    '<ul style="position:absolute; left:0; top:' + $(element[0]).height() + 'px; z-index:1; background-color:white; list-style:none; margin:0; border:solid 1px #ccc;">' +
+                    '<ul ng-show="show" style="position:absolute; left:0; top:' + $(element[0]).height() + 'px; z-index:1; background-color:white; list-style:none; margin:0; border:solid 1px #ccc;">' +
                     '<li style="margin:5px" ng-repeat="option in getData()">{{option}}<li>' +
                     '</ul>');
 
                 $compile(optionsList)(scope);
 
                 element.before(optionsList);
+
+                element.bind('focus', function (event) {
+                    scope.show = true;
+                    scope.$apply();
+                });
+
+                element.bind('blur', function (event) {
+                    scope.show = false;
+                    scope.$apply();
+                });
 
                 //ngModel.$parsers.unshift(function (viewValue) {
                 //    if (viewValue === 'foo') {
@@ -40,6 +53,11 @@
                 //        return viewValue;
                 //    }
                 //});
+
+
+
+
+
 
                 // return the expected array
                 scope.getData = function () {
