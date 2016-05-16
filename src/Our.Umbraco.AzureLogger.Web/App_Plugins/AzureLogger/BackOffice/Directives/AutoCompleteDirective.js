ada57c1bf90fc5601cdc425574875ffbc47f344d
Hendy <git@hendyracher.co.uk>
2016-05-17 00:56:21 +0100

    auto complete list, now filters on input value

--- src/Our.Umbraco.AzureLogger.Web/App_Plugins/AzureLogger/BackOffice/Directives/AutoCompleteDirective.js
+++ src/Our.Umbraco.AzureLogger.Web/App_Plugins/AzureLogger/BackOffice/Directives/AutoCompleteDirective.js
@@ -10,60 +10,54 @@
     function AutoCompleteDirective($compile) {
 
         return {
-            require: 'ngModel',
+            //require: 'ngModel',
             restrict: 'A',
             scope: '=',
             //template: '<ul><li>example autocomplete option</li></ul>',
-            link: function (scope, element, attrs, ngModel) {
+            link: function (scope, element, attrs) {  //, ngModel) {
 
                 // ensure only attached to input element of type text
                 if (element[0].tagName.toLowerCase() !== 'input' || element[0].type !== 'text') { return; }
 
                 scope.show = false;
+                scope.options = [];
+                scope.value = null;
 
                 // build list markup
                 var optionsList = angular.element(
                     '<ul class="auto-complete-directive" ng-show="show" style="position:absolute; left:0; top:' + $(element[0]).height() + 'px; z-index:1; background-color:white; list-style:none; margin:0; border:solid 1px #ccc;">' +
-                    '<li style="margin:5px" ng-repeat="option in getData()">{{option}}<li>' +
+                    '<li style="margin:5px" ng-repeat="option in options | filter:value ">{{option}}<li>' +
                     '</ul>');
 
+                // compile list markup with scope
                 $compile(optionsList)(scope);
 
+                // add list markup to dom
                 element.before(optionsList);
 
+                // show list
                 element.bind('focus', function (event) {
                     scope.show = true;
                     scope.$apply();
                 });
 
+                // hide list
                 element.bind('blur', function (event) {
                     scope.show = false;
                     scope.$apply();
                 });
 
-                //ngModel.$parsers.unshift(function (viewValue) {
-                //    if (viewValue === 'foo') {
-                //        // revert to previous (model value)
-                //        var currentValue = ngModel.$modelValue;
-                //        ngModel.$setViewValue(currentValue);
-                //        ngModel.$render();
-                //        return currentValue;
-                //    }
-                //    else {
-                //        return viewValue;
-                //    }
-                //});
 
+                // http://stackoverflow.com/questions/19167517/angularjs-in-a-directive-that-changes-the-model-value-why-do-i-have-to-call
+                // using watch (rather than $parsers) as not changing the value being watched
+                scope.$watch(attrs.ngModel, function (value) {
+                    scope.value = value;
+                });
 
-
-
-
-
-                // return the expected array
-                scope.getData = function () {
-                    return scope.$eval(attrs.autoComplete);
-                }
-
+                // wait until autocomplete data is set (currently set in parent after an ajax call)
+                scope.$watch(attrs.autoComplete, function (value) {
+                    scope.options = value;
+                });
             }
         }
     }
