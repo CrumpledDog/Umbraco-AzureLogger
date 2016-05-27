d15f8013c4efd3990d4eea00dedada1918402212
Jeavon <jeavon@crumpled-dog.com>
2016-05-27 13:38:33 +0100

    Adding installer configuration wizard

---
+++ src/Our.Umbraco.AzureLogger.Installer/Configurator/Controllers/Configure.js
@@ -0,0 +1,30 @@
+﻿var configApp = angular.module('AzureLoggerLoader', []);
+
+configApp.controller("Loader", function ($scope, $http, $log) {
+  var postDataUrl = "/Umbraco/backoffice/AzureLogger/Installer/PostParameters";
+
+    $scope.saved = false;
+
+    $scope.submitForm = function (e) {
+        e.preventDefault();
+
+        $http.post(postDataUrl, JSON.stringify($scope.connectionString)).success(function (data) {
+
+            var status;
+            if (typeof data === "string") {
+                status = JSON.parse(data);
+            } else {
+                status = data;
+            }
+
+            $scope.status = status;
+
+            if (status !== "ConnectionError") {
+                $scope.saved = true;
+            }
+
+        });
+
+    }
+
+});
---
+++ src/Our.Umbraco.AzureLogger.Installer/InstallerController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web.Hosting;
+using System.Web.Http;
+using System.Xml;
+using Microsoft.WindowsAzure.Storage;
+using Our.Umbraco.AzureLogger.Installer.Enums;
+using umbraco.cms.businesslogic.packager.standardPackageActions;
+using Umbraco.Core;
+using Umbraco.Core.Logging;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+
+namespace Our.Umbraco.AzureLogger.Installer
+{
+    [PluginController("AzureLogger")]
+    public class InstallerController : UmbracoAuthorizedApiController
+    {
+        private readonly string log4netConfigInstallXdtPath = HostingEnvironment.MapPath(string.Format("{0}{1}.install.xdt", global::Our.Umbraco.AzureLogger.Installer.Constants.InstallerPath,global::Our.Umbraco.AzureLogger.Installer.Constants.Log4NetConfigFile));
+        private readonly string log4netConfigPath = HostingEnvironment.MapPath(string.Format("{0}{1}", global::Our.Umbraco.AzureLogger.Installer.Constants.UmbracoConfigPath,global::Our.Umbraco.AzureLogger.Installer.Constants.Log4NetConfigFile));
+
+        private readonly string webConfigXdtPath = HostingEnvironment.MapPath(string.Format("{0}{1}.install.xdt", global::Our.Umbraco.AzureLogger.Installer.Constants.InstallerPath,global::Our.Umbraco.AzureLogger.Installer.Constants.WebConfigFile));
+        private readonly string webConfigPath = HostingEnvironment.MapPath(string.Format("~/{0}", global::Our.Umbraco.AzureLogger.Installer.Constants.WebConfigFile));
+
+        // /Umbraco/backoffice/AzureLogger/Installer/PostParameters
+        [HttpPost]
+        public InstallerStatus PostParameters([FromBody]string connectionString)
+        {
+            if (!TestAzureCredentials(connectionString))
+            {
+                return InstallerStatus.ConnectionError;
+            }
+
+
+            return InstallerStatus.SaveXdtError;
+        }
+
+
+        private static bool TestAzureCredentials(string connectionString)
+        {
+
+            try
+            {
+                var cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
+
+                var cloudTableClient = cloudStorageAccount.CreateCloudTableClient();
+               // var blobContainer = cloudBlobClient.GetContainerReference(containerName);
+                var tableList = cloudTableClient.ListTables();
+                // this should fully check that the connection works, the result is not relevant
+
+            }
+            catch (Exception e)
+            {
+                LogHelper.Error<InstallerController>(string.Format("Error validating Azure storage connection: {0}", e.Message), e);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
