6cf2e36fe2614294bd56ef352326e928f203ea6b
Hendy Racher <git@hendyracher.co.uk>
2017-01-06 20:15:03 +0000

    empty view & controller

---
+++ src/Our.Umbraco.AzureLogger.Web/App_Plugins/AzureLogger/BackOffice/Controllers/ConfigurationController.js
@@ -0,0 +1,24 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('umbraco')
+        .controller('AzureLogger.ConfigurationController', ConfigurationController);
+
+    ConfigurationController.$inject = ['$scope', 'navigationService', 'AzureLogger.AzureLoggerResource'];
+
+    function ConfigurationController($scope, navigationService, azureLoggerResource) {
+
+        var appenderName;
+
+        $scope.init = init;
+ 
+        // --------------------------------------------------------------------------------
+
+        function init() {
+            
+        }
+
+    }
+
+})();
